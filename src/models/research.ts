bfeb5f4738e220124dc21a3b1da903ca4792a7e8
dta181104 <165494716+dta181104@users.noreply.github.com>
2025-04-16 18:35:45 +0700

    Chinh sua model research

--- src/models/research.ts
+++ src/models/research.ts
@@ -1,13 +1,25 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import slugify from 'slugify';
+
 
 const researchSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  research_id: { type: String, required: true, unique: true },
   type: { type: String, required: true },
+  title: { type: String, required: true },
+  slug: { type: String, required: false },
+  date: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String },
   detail: { type: String, required: true },
-  date: { type: String},
 });
+
+researchSchema.pre('save', function (this: any, next: () => void) {
+  if (this.isModified('title') || !this.slug) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  next();
+});
+
 
 const Research = mongoose.model("Research", researchSchema);
 export default Research;
